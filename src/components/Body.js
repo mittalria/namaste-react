fc65b8d5dee22eb4cd447c72a56553570c224c6d
Ria Dang <mittalria16@gmail.com>
2026-02-11 16:46:47 +0530

    episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,19 @@
 import RestrauntCard from "./RestrauntCard";
 import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestraunts, setListOfRestraunts] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredRestraunts, setFilteredRestraunts] = useState([]);
+
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=80382&tags=layout_CCS_CholeBhature&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&collection=80382&tags=layout_CCS_CholeBhature&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
     );
 
     const json = await data.json();
@@ -17,11 +21,36 @@ const Body = () => {
       (item) => item?.card?.card.info
     );
     setListOfRestraunts(filteredJson);
+    setFilteredRestraunts(filteredJson);  
   };
 
-  return (
+  return listOfRestraunts.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const filteredRestraunt = listOfRestraunts.filter((res) =>
+                res?.card?.card?.info?.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase())
+              );
+              setFilteredRestraunts(filteredRestraunt);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
@@ -35,7 +64,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestraunts.map((restraunt) => (
+        {filteredRestraunts.map((restraunt) => (
           <RestrauntCard
             key={restraunt?.card?.card?.info?.id}
             resData={restraunt?.card?.card}
